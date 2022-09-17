2a59c46d01c2a242e9abadc881e7042e5cfe717f
mcantop <maciejcantop@icloud.com>
2022-09-17 14:15:51 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maciej on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
